83f82cf5d01f3c53d01ad6662d4f6eeb27ac1977
gui25 <gui25ber@gmail.com>
2025-06-20 19:26:00 -0300

    refactor: remove optional description field from monster and session pages to streamline content presentation

--- app/admin/edit/monster/[slug]/page.tsx
+++ app/admin/edit/monster/[slug]/page.tsx
@@ -407,10 +407,6 @@ export default function EditMonsterPage({ params }: { params: Promise<{ slug: st
                     </div>
                   </div>
 
-                  {/* Descrição opcional */}
-                  {mockFrontmatter.description && (
-                    <div className="mt-4 italic text-gray-300">"{mockFrontmatter.description}"</div>
-                  )}
                 </div>
 
                 {/* Main content com as mesmas classes da página real */}
--- app/admin/edit/session/[slug]/page.tsx
+++ app/admin/edit/session/[slug]/page.tsx
@@ -394,10 +394,6 @@ export default function EditSessionPage({ params }: { params: Promise<{ slug: st
                     </div>
                   </div>
 
-                  {/* Descrição opcional */}
-                  {mockFrontmatter.description && (
-                    <div className="mt-4 italic text-gray-300">"{mockFrontmatter.description}"</div>
-                  )}
                 </div>
 
                 <article 
